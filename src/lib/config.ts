ed5409598e38bfced1efb09692cbc4cf62ef4fb1
RanimeV3 <coinmana30@gmail.com>
2026-01-01 19:16:43 +0700

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,6 @@
 // API Configuration
 export const API_CONFIG = {
-  BASE_URL: 'https://www.sankavollerei.com',
+  BASE_URL: 'https://sakananime.my.id',
   SITE_URL: window.location.origin,
 } as const;
 
