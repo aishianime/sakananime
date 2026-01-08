b1a69916480695ffeb62cf80438d6e0129efd2b1
RanimeV3 <coinmana30@gmail.com>
2026-01-08 21:05:20 +0700

    Update ComicCard.tsx

--- src/components/ComicCard.tsx
+++ src/components/ComicCard.tsx
@@ -39,7 +39,7 @@ export const ComicCard = ({
 
   return (
     <Link 
-      to={`/comic/bacakomik/detail/${slug}`} 
+      to={`/comic/detail/${slug}`} 
       className="group block focus:outline-none focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2 focus-visible:rounded-lg"
       aria-label={`Baca ${title} - ${chapter || 'komik terbaru'}`}
     >
